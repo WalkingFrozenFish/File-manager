561f98f3e05cd1c83781cdb3dc6c74d46a986833
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-01-15 21:10:08 +0600

    Добавил стили

--- file manager/frontend/src/App.jsx
+++ file manager/frontend/src/App.jsx
@@ -34,37 +34,36 @@ function App() {
         setData(result);
       },
         (error) => {
-
+          console.log(error);
         })
   }
 
   return (
     <div className="file-manager">
       <div>
-        <a href={parent} onClick={clickHandler}>
-          level up
+        <a href={parent} onClick={clickHandler} className="level-up">
+          <span class="material-icons">keyboard_return</span>
+          Level up
         </a>
       </div>
       <div className="current-level">
-        current: {data.path === "" ? "/" : data.path
-          // <a href={data.path}>
-          //   {data.path}
-          // </a>
-        }
+        current: {data.path === "" ? "/" : data.path}
       </div>
       <ul className="folder-list">
         {data.files.map(item => {
-
           if (item.dir) {
             return <li key={item.name} className="folder">
               <a href={data.path + "/" + item.name} onClick={clickHandler}>
+                <span class="material-icons">folder</span>
                 {item.name.toUpperCase()}
               </a>
             </li>;
           } else {
-            return <li key={item.name} className="file">{item.name}</li>;
+            return <li key={item.name} className="file">
+              <span class="material-icons">description</span>
+              {item.name}
+            </li>;
           }
-
         })}
       </ul>
     </div>
